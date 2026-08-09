0b984ef963fcc192335a4b697dd971eb4f30162c
Hanxi <hanxi@lakesidernd.com>
2026-08-10 01:56:47 +0800

    Configure frontend API URL for deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -4,11 +4,7 @@ import axios from "axios";
 const api = axios.create({
 
     baseURL:
-        "http://127.0.0.1:8000",
-
-    headers:{
-        "Content-Type":"application/json"
-    }
+        import.meta.env.VITE_API_URL
 
 });
 
